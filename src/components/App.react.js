9d6ce71e221fe002db2cf1ebb6ff2a9397dbace2
Hafiz Ismail <hafiz@wehavefaces.net>
2016-04-22 01:01:50 +0800

    Fleshed out app by adding handlers for socket events. - When certain event happens, we'll update the app states accordingly - For e.g, when we receive a new message, we add the message to the `messages` list stored in our state

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -37,28 +37,114 @@ export class App extends React.Component {
     socket.on("connect", this.handleConnect.bind(this));
     socket.on("disconnect", this.handleConnect.bind(this));
 
+    //welcome message received from the server
+    socket.on("welcome", this.handleWelcome.bind(this));
+
+    //chat message from another user
+    socket.on("chat", this.handleChat.bind(this));
+
+    //message received that new user has joined the chat
+    socket.on("joined", this.handleJoined.bind(this));
+
+    //handle leaving message
+    socket.on("left", this.handleLeft.bind(this));
+
+    //keep track of who is online
+    socket.on("online", this.handleTrackOnline.bind(this));
+
   }
   // Invoked immediately before a component is unmounted from the DOM.
   componentWillUnmount() {
 
     socket.off("connect");
     socket.off("disconnect");
+    socket.off("welcome");
+    socket.off("chat");
+    socket.off("joined");
+    socket.off("left");
+    socket.off("online");
 
+  }
+
+  /* React component custom methods */
+
+  addMessage(message) {
+    let messages = this.state.messages;
+    messages.push(message);
+    this.setState({
+      messages: messages
+    })
   }
 
   /* React component event handler methods */
 
   handleConnect() {
     console.log("Connected to Chat Socket");
+
     this.setState({
       isConnected: true
     });
   }
-  
   handleDisconnect() {
     console.log("Disconnected from Chat Socket");
+
     this.setState({
       isConnected: false
+    });
+  }
+  handleWelcome(msg) {
+    console.log("Received welcome message: ", msg);
+    this.addMessage({
+      type: "welcome_message",
+      message: msg
+    });
+
+    //  successfully joined chat, so we hide join form and show chat panel
+    this.setState({
+      disableJoinButton: false,
+      showJoinForm: false,
+      disableChatButton: false,
+      showChatPanel: true
+    });
+  }
+  handleChat(msg) {
+    console.log("Received message: ", msg);
+
+    this.addMessage({
+      type: "chat_message",
+      username: msg.user.name,
+      message: msg.message
+    });
+  }
+  handleJoined(user) {
+    console.log(user.name + " joined the chat.");
+
+    this.addMessage({
+      type: "join_message",
+      username: user.name
+    });
+  }
+  handleLeft(user) {
+    console.log(user.name + " left the chat.");
+
+    this.addMessage({
+      type: "left_message",
+      username: user.name
+    });
+  }
+  handleTrackOnline(connections) {
+    console.log("Connections: ", connections);
+
+    // collect usernames of users thats connected to the server
+    var users = [];
+    for (var i=0; i< connections.length; ++i){
+      if ( connections[i].user ) {
+        users.push(connections[i].user.name);
+      }
+    }
+
+    this.setState({
+      users: users
     });
   }
 
